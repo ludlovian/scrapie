95cd9a0796fb159b8fd23c1bdcc68d147ded68f4
Alan Layng <alan@readersludlow.com>
2023-06-04 22:09:57 +0100

    Give each instance its own Regex to enable re-entrancy

--- src/parser.mjs
+++ src/parser.mjs
@@ -1,4 +1,4 @@
-const rgxMain = (() => {
+function makeRgxMain () {
   const textElement = '(?<=^|>)' + '([^<]+)' + '(?=<)'
   const cdata = '<!\\[CDATA\\['
   const comment = '<!--'
@@ -6,7 +6,21 @@ const rgxMain = (() => {
   const specialElement = '(' + cdata + '|' + script + '|' + comment + ')'
   const tagElement = '(?:<)' + '([^>]*)' + '(?:>)'
   return new RegExp(textElement + '|' + specialElement + '|' + tagElement, 'g')
-})()
+}
+
+function makeRgxTag () {
+  const maybeClose = '(\\/?)'
+  const typeName = '(\\S+)'
+  const elementType = `^${maybeClose}${typeName}`
+  const attrName = '(\\S+)'
+  const attrValueDQ = '"([^"]*)"'
+  const attrValueSQ = "'([^']*)'"
+  const attrValueNQ = '([^\\s/]+)'
+  const attrValue = `(?:${attrValueDQ}|${attrValueSQ}|${attrValueNQ})`
+  const attr = `${attrName}\\s*=\\s*${attrValue}`
+  const selfClose = '(\\/)\\s*$'
+  return new RegExp(`${elementType}|${attr}|${selfClose}`, 'g')
+}
 
 const specials = {
   '<![CDATA[': { rgx: /]]>/, start: 9, end: 3, emit: 'cdata' },
@@ -16,94 +30,82 @@ const specials = {
 
 const CHUNK = 1024
 
-const rgxTag = (() => {
-  const maybeClose = '(\\/?)'
-  const typeName = '(\\S+)'
-  const elementType = `^${maybeClose}${typeName}`
-  const attrName = '(\\S+)'
-  const attrValueDQ = '"([^"]*)"'
-  const attrValueSQ = "'([^']*)'"
-  const attrValueNQ = '(\\S+)'
-  const attrValue = `(?:${attrValueDQ}|${attrValueSQ}|${attrValueNQ})`
-  const attr = `${attrName}\\s*=\\s*${attrValue}`
-  const selfClose = '(\\/)\\s*$'
-  return new RegExp(`${elementType}|${attr}|${selfClose}`, 'g')
-})()
-
 export default class Parser {
   constructor (handler) {
-    this.buffer = ''
-    this.special = false
-    this.handler = handler
+    this._buffer = ''
+    this._special = false
+    this._emit = handler
+    this._rgxMain = makeRgxMain()
+    this._rgxTag = makeRgxTag()
   }
 
   write (text) {
-    this.buffer += text
-    if (this.special) handleSpecial(this)
-    else handle(this)
+    this._buffer += text
+    if (this._special) this._handleSpecial()
+    else this._handle()
   }
-}
 
-function handle (p) {
-  rgxMain.lastIndex = undefined
-  let consumed = 0
-  while (true) {
-    const m = rgxMain.exec(p.buffer)
-    if (!m) break
-    const [, text, special, tag] = m
-    if (text) {
-      consumed = m.index + text.length
-      p.handler({ text })
-    } else if (tag) {
-      consumed = m.index + tag.length + 2
-      p.handler(parseTag(tag))
-    } else if (special) {
-      p.special = special
-      const { start } = specials[special]
-      consumed = m.index + start
-      p.buffer = p.buffer.slice(consumed)
-      return handleSpecial(p)
+  _handle () {
+    this._rgxMain.lastIndex = undefined
+    let consumed = 0
+    while (true) {
+      const m = this._rgxMain.exec(this._buffer)
+      if (!m) break
+      const [, text, special, tag] = m
+      if (text) {
+        consumed = m.index + text.length
+        this._emit({ text })
+      } else if (tag) {
+        consumed = m.index + tag.length + 2
+        this._emit(this._parseTag(tag))
+      } else if (special) {
+        this._special = special
+        const { start } = specials[special]
+        consumed = m.index + start
+        this._buffer = this._buffer.slice(consumed)
+        return this._handleSpecial()
+      }
+    }
+    this._buffer = this._buffer.slice(consumed)
+  }
+
+  _handleSpecial () {
+    const { rgx, end, emit } = specials[this._special]
+    const match = rgx.exec(this._buffer)
+    if (match) {
+      const data = this._buffer.slice(0, match.index)
+      this._buffer = this._buffer.slice(match.index + end)
+      if (emit && data.length) this._emit({ [emit]: data })
+      this._special = false
+      return this._handle()
+    }
+    if (this._buffer.length > CHUNK) {
+      const data = this._buffer.slice(0, CHUNK)
+      this._buffer = this._buffer.slice(CHUNK)
+      if (emit) this._emit({ [emit]: data })
     }
   }
-  p.buffer = p.buffer.slice(consumed)
-}
 
-function handleSpecial (p) {
-  const { rgx, end, emit } = specials[p.special]
-  const match = rgx.exec(p.buffer)
-  if (match) {
-    const data = p.buffer.slice(0, match.index)
-    p.buffer = p.buffer.slice(match.index + end)
-    if (emit && data.length) p.handler({ [emit]: data })
-    p.special = false
-    return handle(p)
-  }
-  if (p.buffer.length > CHUNK) {
-    const data = p.buffer.slice(0, CHUNK)
-    p.buffer = p.buffer.slice(CHUNK)
-    if (emit) p.handler({ [emit]: data })
-  }
-}
-
-function parseTag (tag) {
-  if (tag.startsWith('!') || tag.startsWith('?')) return { meta: tag }
-  const out = { type: '' }
-  rgxTag.lastIndex = undefined
-  while (true) {
-    const m = rgxTag.exec(tag)
-    if (!m) return out
-    const [, close, type, name, dq, sq, nq, selfClose] = m
-    if (type) {
-      out.type = type
-      if (close) {
-        out.close = true
-      } else {
-        out.attrs = {}
+  _parseTag (tag) {
+    if (tag.startsWith('!') || tag.startsWith('?')) return { meta: tag }
+    const out = { type: '' }
+    this._rgxTag.lastIndex = undefined
+    while (true) {
+      const m = this._rgxTag.exec(tag)
+      if (!m) return out
+      const [, close, type, name, dq, sq, nq, selfClose] = m
+      if (type) {
+        out.type = type
+        if (close) {
+          out.close = true
+        } else {
+          out.attrs = {}
+        }
+      } else if (name) {
+        out.attrs[name] = dq || sq || nq
+      } else if (selfClose) {
+        out.selfClose = true
       }
-    } else if (name) {
-      out.attrs[name] = dq || sq || nq
-    } else if (selfClose) {
-      out.selfClose = true
     }
   }
 }
